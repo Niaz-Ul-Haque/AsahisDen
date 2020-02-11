65b2eb187c1034b17a812e81fcaf011d1ccf5d0a
Niaz Ul Haque <theniaz619.2@gmail.com>
2020-02-11 16:38:19 -0500

    Done with my homefront unless i make everything dynam, so far its statically complete

--- model/category.js
+++ model/category.js
@@ -1,9 +1,9 @@
 const category = {
 
-    categoryDB: [{title:'Shirt A',description:`Best Summer Shirt`,price:`13.99`, imgSrc: "/img/category 1.jpeg"},
-    {title:'Shirt C',description:`Best Summer Shirt`,price:`49.99`, imgSrc: "/img/category 3.jpg"},
-    {title:'Shirt B',description:`Best Summer Shirt.`,price:`17.99`, imgSrc: "/img/category 2.jpg"},
-    {title:'Shirt D',description:`Best Summer Shirt`,price:`49.99`, imgSrc: "/img/category 4.jpg"}],
+    categoryDB: [{title:'Shirt A',description:`Best Summer Shirt`,price:`13.99`, imgSrc: "/img/category 1.jpeg", name: "Kids"},
+    {title:'Shirt C',description:`Best Summer Shirt`,price:`49.99`, imgSrc: "/img/category 3.jpg", name: "Teens"},
+    {title:'Shirt B',description:`Best Summer Shirt.`,price:`17.99`, imgSrc: "/img/category 2.jpg", name: "Male"},
+    {title:'Shirt D',description:`Best Summer Shirt`,price:`49.99`, imgSrc: "/img/category 4.jpg", name: "Female"}],
 
     getAllProducts() {
         return this.categoryDB;
